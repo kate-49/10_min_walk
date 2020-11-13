fba090a67ef9e99e5904a952a52c20b85ca5c532
kate smith <kateesmith4994@gmail.com>
2020-11-13 16:04:18 +0000

    refactored

--- walk.go
+++ walk.go
@@ -3,11 +3,11 @@ package walk
 func Run(directions []string) bool {
 
 	var answer bool
+	var n, s, e, w int
 
 	if len(directions) != 10 {
 		answer = false
 	} else {
-		var n, s, e, w int
 		for _, letter := range directions {
 			switch letter {
 			case "n":
